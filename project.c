4da136de68fb736ceb363ef77bfa0dcbddf6d9c7
Alessandro Papi - STUDENT <g00475903@atu.ie>
2026-03-19 17:32:10 +0000

    added random number gen

--- project.c
+++ project.c
@@ -1,70 +1,74 @@
 // 26 - 02 - 2026
+//Alessandro
+//Project
+//19_03_2026
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
 #include <string.h>
+
 int main()
 {
+    char displayGrid[6][6];
+    int values[6][6];
+    int row, col, size = 6;
 
-	
-	char grid[6][6];
-	int row, col,size = 6;
-	
-	int locationX, locationY;
+    int locationX, locationY;
 
-	char password[] = "Alex07";
-	char input[10];
+    char password[] = "Alex07";
+    char input[10];
+
+    for (int i = 0; i < 3; i++) {
+        printf("Enter the password: ");
+        fgets(input, sizeof(input), stdin);
+
+        if (input[strlen(input) - 1] == '\n') {
+            input[strlen(input) - 1] = '\0';
+        }
+
+        if (strcmp(password, input) == 0) {
+            printf("Correct password! The game starts now.\n");
+            break;
+        }
+        else {
+            if (i == 2) {
+                printf("Wrong password. No attempts left. Exit.\n");
+                return 0;
+            }
+            else {
+                printf("Wrong password... do you have Alzheimer's or something?\n");
+            }
+        }
+    }
+
+    srand((int)time(0));
 
 
-	for (int i = 0; i < 3; i++) {
+    for (row = 0; row < size; row++) {
+        for (col = 0; col < size; col++) {
+            values[row][col] = rand() % 256;
+            displayGrid[row][col] = '$';
+        }
+    }
 
-		printf("Enter the password: ");
-		fgets(input, sizeof(input), stdin);
 
-		if (input[strlen(input) - 1] == '\n') {
-			input[strlen(input) - 1] = '\0';
-		}
+    printf("\Grid:\n");
+    for (row = 0; row < size; row++) {
+        printf("%d\t", row);
+        for (col = 0; col < size; col++) {
+            printf("[%d][%d] %c ", row, col, displayGrid[row][col]);
+        }
+        printf("\n");
+    }
+    printf("\t1\t2\t3\t4\t5\t6\n");
 
-		if (strcmp(password, input) == 0) {
-			printf("Correct password! The game starts now.\n");
+    printf("choose a location [x]: ");
+    scanf_s("%d", &locationX);
+    printf("choose a location [y]: ");
+    scanf_s("%d", &locationY);
 
-			break;
-		}
-		else {
-			if (i == 2) {
-				printf("Wrong password. No attempts left. Exit.\n");
-				return 0;
-			}
-			else {
-				printf("Wrong password... do you have Alzheimer's or something?\n");
+    printf("You just choose [%d][%d] the real number is: %d\n", locationX, locationY, values[locationX][locationY]);
 
-			}
-		}
-	}
-	
-
-	for (row = 0; row < size; row++) {
-		printf("%d\t", row, grid[0][row]);
-		for (col = 0; col < size; col++) {
-			
-			printf("[%d][%d] $",row,col,grid[row][col]);
-			if (col == size - 1) {
-				printf("\n");
-			}
-		}
-	}
-	printf("\t1\t 2\t 3\t 4\t 5\t 6\n");
-
-	printf("choose a location [x] ");
-	scanf_s(&locationX);
-
-	printf("choose a location [y]");
-	scanf_s(&locationY);
-
-	printf("test");
-
-	printf("test2");
-
-	return(0);
+    return 0;
 }
