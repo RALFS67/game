3ccdb983b7aed85bdad1f1606d7493ccc6badb7e
Ralfs Vilsars - STUDENT <g00483357@atu.ie>
2026-03-19 18:27:54 +0000

    added a gold/bomb check and display to grid added value of gold

--- project.c
+++ project.c
@@ -14,10 +14,11 @@ int main()
     char displayGrid[6][6];
     uint8_t values[6][6];
     int row, col, size = 6;
+    int gold = 0;
 
     int locationX, locationY;
 
-    char password[] = "Alex07";
+    char password[] = "Alex07"; //ale
     char input[10];
 
     for (int i = 0; i < 3; i++) {
@@ -51,7 +52,7 @@ int main()
             values[row][col] = rand() % 256;
             displayGrid[row][col] = '$';
         }
-    }
+    } //ale
 
 
     printf("\Grid:\n");//Ralf
@@ -64,17 +65,44 @@ int main()
     }
     printf("\t1\t2\t3\t4\t5\t6\n");
 
-    printf("choose a location [x]: ");
-    scanf_s("%d", &locationX);
-    printf("choose a location [y]: ");
-    scanf_s("%d", &locationY);
 
-    printf("You just choose [%d][%d] the real number is: %d\n", locationX, locationY, values[locationX][locationY]);
-    //Ralf
-    
-    if (values[row][col] & 2) {
+    for (int loop = 0; loop < 18; loop++) {
+        printf("choose a location [x]: ");
+        scanf_s("%d", &locationX);
+        printf("choose a location [y]: ");
+        scanf_s("%d", &locationY);
 
+        printf("You just choose [%d][%d] the real number is: %d\n", locationX, locationY, values[locationX][locationY]);
+        //Ralf
+
+        if (values[locationX][locationY] & 1) {//ralfs
+
+            displayGrid[locationX][locationY] = 'B';
+
+        }
+
+        else if (values[locationX][locationY] & 2) {
+            displayGrid[locationX][locationY] = 'G';
+            values[locationX][locationY] = (values[locationX][locationY] >> 4);
+            gold = values[locationX][locationY];
+        }
+
+        else
+        {
+            displayGrid[locationX][locationY] = 'E';
+
+        }
+
+        printf("\Grid:\n");//Ralf
+        for (row = 0; row < size; row++) {
+            printf("%d\t", row);
+            for (col = 0; col < size; col++) {
+                printf("[%d][%d] %c ", row, col, displayGrid[row][col]);
+            }
+            printf("\n");
+        }
+        printf("\t1\t2\t3\t4\t5\t6\n");
+        printf("\nyou found %i gold", gold);//ralfs
     }
-
     return 0;
 }
