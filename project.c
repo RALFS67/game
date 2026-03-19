3a9b088ab9ac30391e1872854890396a3feb8f48
Alessandro Papi - STUDENT <g00475903@atu.ie>
2026-03-19 19:47:59 +0000

    Fix merge issues

--- project.c
+++ project.c
@@ -42,7 +42,7 @@ int main()
         }
     }
 
-    srand((int)time(0));
+    srand((int)time(0));//ALE
 
     int currentPlayer;
     int randomStart = rand() % 2;//ALE 
@@ -57,7 +57,7 @@ int main()
             values[row][col] = rand() % 256;
             displayGrid[row][col] = '$';
         }
-    } //ale
+    } //Ale
 
 
     printf("\Grid:\n");//Ralf
@@ -72,6 +72,9 @@ int main()
 
 
     for (int loop = 0; loop < 3; loop++) {
+
+
+        printf("Player %d is your turn \n", currentPlayer); // MULTIPLAYER ALE
         
         do {
 
@@ -89,7 +92,7 @@ int main()
         printf("You just choose [%d][%d] the real number is: %d\n", locationX, locationY, values[locationX][locationY]);
         //Ralf
 
-        if (values[locationX][locationY] & 1) {//ralfs
+        if (values[locationX][locationY] & 1) {//Ralfs
 
             displayGrid[locationX][locationY] = 'B';
 
@@ -118,7 +121,9 @@ int main()
             }
             printf("\n");
         }
-        printf("\t1\t2\t3\t4\t5\t6\n");//ralfs
+        printf("\t1\t2\t3\t4\t5\t6\n");//Ralfs
+        currentPlayer = 3 - currentPlayer; // change player ALE
+
     }
         
     return 0;
